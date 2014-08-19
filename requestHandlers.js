d2abe9a65cf2a4f7e8f80547841795f88f2a411b
hylnecjt <719094881@qq.com>
2014-08-19 19:26:47 +0800

    delete

---
+++ requestHandlers.js
@@ -0,0 +1,29 @@
+/**
+ * Created by mortred on 2014/8/19.
+ */
+var start = function(response, postData){
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+};
+
+var upload = function(response, postData){
+    console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("You've sent: " + postData);
+    response.end();
+};
+exports.start = start;
+exports.upload = upload;
